406ca9f72a3fa6d0a74a5d6f6508ddf2afc32ccc
Marcelo Ceolin <fxsobr@gmail.com>
2017-04-05 10:56:50 -0300

    adicionado canos

--- core/src/br/com/githain/flappybird/FlappyBird.java
+++ core/src/br/com/githain/flappybird/FlappyBird.java
@@ -6,37 +6,57 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.util.Random;
+
 public class FlappyBird extends ApplicationAdapter {
 
 	private  SpriteBatch batch;
 	private Texture[] passaros;
 	private Texture fundo;
+	private Texture canoBaixo;
+	private Texture canoTopo;
+    private Random numeroRandomico;
+
+
 	private int larguraDispositivo;
 	private int alturaDispositivo;
 	private float variacao = 0;
 	private float velocidadeQueda;
 	private float posicaoInicialVertical;
+    private float posicaoMovimentoCanoHorizontal;
+    private float espacoEntreCanos;
+    private float deltaTime;
+    private float alturaCanosRandomica;
 
 	@Override
 	public void create () {
 
 		batch = new SpriteBatch();
+        numeroRandomico = new Random();
 		passaros = new Texture[3];
 		passaros[0] = new Texture("passaro1.png");
 		passaros[1] = new Texture("passaro2.png");
 		passaros[2] = new Texture("passaro3.png");
 
 		fundo = new Texture("fundo.png");
+		canoBaixo = new Texture("cano_baixo.png");
+		canoTopo = new Texture("cano_topo.png");
 
 		larguraDispositivo = Gdx.graphics.getWidth();
 		alturaDispositivo = Gdx.graphics.getHeight();
 		posicaoInicialVertical = alturaDispositivo /2;
+        posicaoMovimentoCanoHorizontal = larguraDispositivo;
+        espacoEntreCanos = 300;
 
 	}
 
 	@Override
 	public void render () {
-		variacao += Gdx.graphics.getDeltaTime() * 10;
+
+        deltaTime = Gdx.graphics.getDeltaTime();
+
+		variacao += deltaTime * 10;
+        posicaoMovimentoCanoHorizontal -= deltaTime * 200;
 		velocidadeQueda++;
 
 		if(variacao > 2) variacao = 0;
@@ -49,15 +69,17 @@ public class FlappyBird extends ApplicationAdapter {
 		if(posicaoInicialVertical > 0 || velocidadeQueda <0 )
 			posicaoInicialVertical -= velocidadeQueda;
 
-
-
+        if(posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()){
+            posicaoMovimentoCanoHorizontal = larguraDispositivo;
+            alturaCanosRandomica = numeroRandomico.nextInt(400) - 200;
+        }
 
 		batch.begin();
 
 		batch.draw(fundo, 0,0 , larguraDispositivo, alturaDispositivo);
-		batch.draw(passaros[(int) variacao], 30, posicaoInicialVertical);
-
-
+        batch.draw(canoTopo, posicaoMovimentoCanoHorizontal, alturaDispositivo /2 + espacoEntreCanos /2 + alturaCanosRandomica);
+        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo /2 - canoBaixo.getHeight() - espacoEntreCanos /2 + alturaCanosRandomica);
+		batch.draw(passaros[(int) variacao], 120, posicaoInicialVertical);
 
 		batch.end();
 	}
