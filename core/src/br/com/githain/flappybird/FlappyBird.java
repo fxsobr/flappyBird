ab720121a0c8ece1cbf9334aa9214341ae6fc700
Marcelo Ceolin <fxsobr@gmail.com>
2017-04-05 21:00:13 -0300

    adicionado pontuação;

--- core/src/br/com/githain/flappybird/FlappyBird.java
+++ core/src/br/com/githain/flappybird/FlappyBird.java
@@ -2,8 +2,10 @@ package br.com.githain.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.util.Random;
@@ -16,10 +18,14 @@ public class FlappyBird extends ApplicationAdapter {
 	private Texture canoBaixo;
 	private Texture canoTopo;
     private Random numeroRandomico;
+    private BitmapFont texto;
 
 
 	private int larguraDispositivo;
 	private int alturaDispositivo;
+    private int iniciaJogo = 0;
+    private int pontuacao;
+
 	private float variacao = 0;
 	private float velocidadeQueda;
 	private float posicaoInicialVertical;
@@ -27,12 +33,16 @@ public class FlappyBird extends ApplicationAdapter {
     private float espacoEntreCanos;
     private float deltaTime;
     private float alturaCanosRandomica;
+    private boolean marcouPonto = false;
 
 	@Override
 	public void create () {
 
 		batch = new SpriteBatch();
         numeroRandomico = new Random();
+        texto = new BitmapFont();
+        texto.setColor(Color.WHITE);
+        texto.getData().setScale(6);
 		passaros = new Texture[3];
 		passaros[0] = new Texture("passaro1.png");
 		passaros[1] = new Texture("passaro2.png");
@@ -54,34 +64,49 @@ public class FlappyBird extends ApplicationAdapter {
 	public void render () {
 
         deltaTime = Gdx.graphics.getDeltaTime();
+        variacao += deltaTime * 10;
+        if (variacao > 2) variacao = 0;
 
-		variacao += deltaTime * 10;
-        posicaoMovimentoCanoHorizontal -= deltaTime * 200;
-		velocidadeQueda++;
+        if (iniciaJogo == 0) {
+            if (Gdx.input.justTouched()) {
+                iniciaJogo = 1;
+            }
+        } else {
+            posicaoMovimentoCanoHorizontal -= deltaTime * 200;
+            velocidadeQueda++;
 
-		if(variacao > 2) variacao = 0;
-
-		if (Gdx.input.justTouched()){
-			velocidadeQueda = -15;
-		}
+            if (Gdx.input.justTouched()) {
+                velocidadeQueda = -15;
+            }
 
 
-		if(posicaoInicialVertical > 0 || velocidadeQueda <0 )
-			posicaoInicialVertical -= velocidadeQueda;
+            if (posicaoInicialVertical > 0 || velocidadeQueda < 0)
+                posicaoInicialVertical -= velocidadeQueda;
 
-        if(posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()){
-            posicaoMovimentoCanoHorizontal = larguraDispositivo;
-            alturaCanosRandomica = numeroRandomico.nextInt(400) - 200;
+            if (posicaoMovimentoCanoHorizontal < -canoTopo.getWidth()) {
+                posicaoMovimentoCanoHorizontal = larguraDispositivo;
+                alturaCanosRandomica = numeroRandomico.nextInt(400) - 200;
+                marcouPonto = false;
+            }
+
+
+            if(posicaoMovimentoCanoHorizontal < 120){
+                if (!marcouPonto){
+                    pontuacao++;
+                    marcouPonto = true;
+                }
+            }
+
+
         }
+        batch.begin();
 
-		batch.begin();
+        batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+        batch.draw(canoTopo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 + espacoEntreCanos / 2 + alturaCanosRandomica);
+        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + alturaCanosRandomica);
+        batch.draw(passaros[(int) variacao], 120, posicaoInicialVertical);
+        texto.draw(batch, String.valueOf(pontuacao), larguraDispositivo /2, alturaDispositivo - 50);
 
-		batch.draw(fundo, 0,0 , larguraDispositivo, alturaDispositivo);
-        batch.draw(canoTopo, posicaoMovimentoCanoHorizontal, alturaDispositivo /2 + espacoEntreCanos /2 + alturaCanosRandomica);
-        batch.draw(canoBaixo, posicaoMovimentoCanoHorizontal, alturaDispositivo /2 - canoBaixo.getHeight() - espacoEntreCanos /2 + alturaCanosRandomica);
-		batch.draw(passaros[(int) variacao], 120, posicaoInicialVertical);
-
-		batch.end();
-	}
-
+        batch.end();
+        }
 }
